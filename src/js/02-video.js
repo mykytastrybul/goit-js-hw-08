252f14f4294d65593f6e6013bfb9af1e83376d2f
s3bull <95282477+s3bull@users.noreply.github.com>
2022-02-20 09:37:30 +0200

    hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
-
+import throttle from "lodash.throttle";
+const STORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const onPlay = function (data) {
+    console.log(data);
+    onTimeupdate(data);
+};
+player.on("timeupdate", throttle(onPlay, 1000));
+function onTimeupdate(evt) {
+    const playTime = evt.seconds;
+    localStorage.setItem(STORAGE_KEY, playTime);
+}
+function gettingTime() {
+    const savedTime = +localStorage.getItem(STORAGE_KEY);
+    if (savedTime) {
+        console.log(savedTime);
+        player
+            .setCurrentTime(savedTime)
+            .then(function (seconds) {
+            })
+            .catch(function (error) {
+                switch (error.name) {
+                    case "RangeError":
+                        break;
+                    default:
+                        break;
+                }
+            });
+    }
+}
+gettingTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
-
+import throttle from 'lodash.throttle';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+getSavedInputs();
+function getSavedInputs() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+    if (parsedData.email) {
+        refs.email.value = parsedData.email;
+    }
+    if (parsedData.message) {
+        refs.message.value = parsedData.message;
+    }
+    formData = parsedData;
+}
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+    formData = {};
+}
